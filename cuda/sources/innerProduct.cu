259c7d957faa8239d4ec6f6094e2c350fff71e30
17742172132 <17742172132@163.com>
2023-03-19 11:27:28 +0800

    inner product cuda/cublas add

--- cuda/sources/innerProduct.cu
+++ cuda/sources/innerProduct.cu
@@ -4,56 +4,112 @@
 #include <vector>
 #include <chrono>
 
-#define N 4000 // Size of vectors
-
+#define N 10e7 // Size of vectors
+#define ceil(x,y) (((x)+(y)-1)/(y))                                             
 // Kernel function to compute the inner product of two vectors on GPU
-__global__ void innerProductKernel(float* A, float* B, float* result) {
-    __shared__ float shared_mem[256];
-
+__global__ void innerProductKernel(const float* A, const float* B, float* partialSums) {
+    __shared__ float temp[256];
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    shared_mem[threadIdx.x] = (idx < N) ? A[idx] * B[idx] : 0;
-
+    temp[threadIdx.x] = A[idx] * B[idx];
     __syncthreads();
 
-    if (threadIdx.x == 0) {
-        float block_sum = 0;
-        for (int i = 0; i < blockDim.x; i++) {
-            block_sum += shared_mem[i];
+    // Reduce within block
+    for (int i = blockDim.x / 2; i > 0; i >>= 1) {
+        if (threadIdx.x < i) {
+            temp[threadIdx.x] += temp[threadIdx.x + i];
         }
-        atomicAdd(result, block_sum);
+        __syncthreads();
+    }
+    
+    // Store partial sum for block
+    if (threadIdx.x == 0) {
+        partialSums[blockIdx.x] = temp[0];
     }
 }
 
-// Function to compute the inner product of two vectors using CUDA
-float innerProductCUDA(float* A, float* B) {
+float innerProductCUDA(const float* A, const float* B) {
     float* d_A;
     float* d_B;
-    float* d_result;
-    float result = 0;
+    float* d_partialSums;
+
+    float *partialSums = new float [N/256+1];
+
+    // float partialSums[N / 256 + 1] = {0};
+    float h_C = 0;
 
     cudaMalloc(&d_A, N * sizeof(float));
     cudaMalloc(&d_B, N * sizeof(float));
-    cudaMalloc(&d_result, N * sizeof(float));
+    cudaMalloc(&d_partialSums, ceil(N, 256) * sizeof(float));
+
+    dim3 dimBlock(256, 1, 1);
+    dim3 dimGrid(ceil(N, dimBlock.x), 1, 1);
+
+    auto start_cuda = std::chrono::high_resolution_clock::now();
+
+
 
     cudaMemcpy(d_A, A, N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, B, N * sizeof(float), cudaMemcpyHostToDevice);
 
-    dim3 dimBlock(256, 1, 1);
-    dim3 dimGrid((N + dimBlock.x - 1) / dimBlock.x, 1, 1);
+    innerProductKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_partialSums);
 
-    innerProductKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_result);
+    cudaMemcpy(partialSums, d_partialSums, ceil(N, 256) * sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(&result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
-
+    for (int i = 0; i < ceil(N, dimBlock.x); i++) {
+        h_C += partialSums[i];
+    }
+    auto stop_cuda = std::chrono::high_resolution_clock::now();
+    auto duration_cuda = std::chrono::duration_cast<std::chrono::microseconds>(stop_cuda - start_cuda);
+    std::cout << "CUDA_REAL: " << duration_cuda.count() <<" microseconds"<<std::endl;
     cudaFree(d_A);
     cudaFree(d_B);
-    cudaFree(d_result);
+    cudaFree(d_partialSums);
+
+    return h_C;
+}
+
+
+
+
+
+float cublas_dot_product(float* x, float* y, int Narr)
+{
+    // Initialize cublas handle
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    // Allocate memory on the device for the vectors
+    float* d_x, * d_y;
+    cudaMalloc(&d_x, Narr * sizeof(float));
+    cudaMalloc(&d_y, Narr * sizeof(float));
+
+
+    auto start_cublas = std::chrono::high_resolution_clock::now();
+    
+
+    // Copy the vectors from host to device
+    cudaMemcpy(d_x, x, Narr * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, Narr * sizeof(float), cudaMemcpyHostToDevice);
+
+    // Calculate the inner product of the vectors using cublas
+    float result;
+    cublasSdot(handle, Narr, d_x, 1, d_y, 1, &result);
+    auto stop_cublas = std::chrono::high_resolution_clock::now();
+    auto duration_cublas = std::chrono::duration_cast<std::chrono::microseconds>(stop_cublas - start_cublas);
+    std::cout << "CUBLAS: " <<duration_cublas.count()<<" microseconds\n";
+    // Free memory on the device
+    cudaFree(d_x);
+    cudaFree(d_y);
+
+    // Destroy cublas handle
+    cublasDestroy(handle);
 
     return result;
 }
 
+
 // Function to compute the inner product of two vectors using the CPU
-float innerProductCPU(float* A, float* B) {
+float innerProductCPU(const float* A, const float* B) {
     float result = 0;
     for (int i = 0; i < N; i++) {
         result += A[i] * B[i];
@@ -83,17 +139,22 @@ int main() {
     auto stop_cpu = std::chrono::high_resolution_clock::now();
     auto duration_cpu = std::chrono::duration_cast<std::chrono::microseconds>(stop_cpu - start_cpu);
 
-    // Check if results are equal
-    if (result_cpu == result_cuda) {
-        std::cout << "Results are equal\n";
-    } else {
-        std::cout << "Results are not equal\n";
-        
-    }
+    float result_cublas = cublas_dot_product(A.data(), B.data(),N);
+
+    
     
     // Print execution times
-    std::cout << "CUDA: " << duration_cuda.count() << " microseconds\n";
-    std::cout << "CPU: " << duration_cpu.count() << " microseconds\n";
+    // std::cout << "CUDA: " << duration_cuda.count() << " microseconds\n";
+    
+    
+    std::cout << "CPU: " << duration_cpu.count() << " microseconds\n";  
 
+    // Check if results are equal
+    if (result_cpu == result_cuda && result_cpu == result_cublas) {
+        std::cout << "Results are equal\n";
+    } else {
+        std::cout << "Results are not equal\n "<<result_cublas<<std::endl;
+        
+    }
     return 0;
 }
